d22170386a3f51ee128adceac66c6fe2529240c8
albuquerque-lucas <lucaslpra@gmail.com>
2023-08-25 19:51:43 -0300

    Criando EntityManagerCreator

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -2,11 +2,16 @@
 
 namespace AlbuquerqueLucas\PhpTestRouting\Controller;
 
+use AlbuquerqueLucas\PhpTestRouting\Helper\EntityManagerCreator;
 use AlbuquerqueLucas\PhpTestRouting\Views\ProjectsView;
+use Doctrine\ORM\Exception\EntityManagerClosed;
 
 class ProjectsController {
   public function renderProjectsPublic() {
     $projects = ['Projeto 1', 'Projeto 2', 'Projeto 3'];
+    $entityManager = EntityManagerCreator::create();
+    var_dump($entityManager);
+    exit();
     $view = new ProjectsView();
     $view->assign('title', 'Projetos | Financial Manager');
     $view->assign('projects', $projects);
---
+++ src/Helper/EntityManagerCreator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AlbuquerqueLucas\PhpTestRouting\Helper;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerCreator {
+
+  public static function create()
+  {
+    $config = ORMSetup::createAttributeMetadataConfiguration(
+      paths: array(__DIR__."/src/Entity"),
+      isDevMode: true,
+  );
+    $connection = DriverManager::getConnection([
+      'driver' => 'pdo_mysql',
+      'host' => 'localhost',
+      'user' => 'root',
+      'password' => 'mamao123mamao',
+      'dbname' => 'financial_management',
+    ], $config);
+    $entityManager = new EntityManager($connection, $config);
+    return $entityManager;
+  }
+}
